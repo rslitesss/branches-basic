2696867cbf74af1c8898fcc9b997f6e9d72d6bff
rslitesss <kayashev@bk.ru>
2020-03-13 14:16:55 +0700

    Doubles acceptable number ammount

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 main()
 {
-	int a[5],sum=0,mult=1;
-	for(int i=0;i<5;i++)cin>>a[i],sum+=a[i],mult*=A[i];
+	int a[10],sum=0,mult=1;
+	for(int i=0;i<10;i++)cin>>a[i],sum+=a[i],mult*=A[i];
 	cout<<sum<<" "<<mult;
 }
