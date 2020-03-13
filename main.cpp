4e78c98883755d1bf5da29dc07163ee47cb66390
rslitesss <kayashev@bk.ru>
2020-03-13 14:11:18 +0700

    Adds code to multiply all array members and output it too

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 main()
 {
-	int a[5],sum=0;
-	for(int i=0;i<5;i++)cin>>a[i],sum+=a[i];
-	cout<<sum;
+	int a[5],sum=0,mult=1;
+	for(int i=0;i<5;i++)cin>>a[i],sum+=a[i],mult*=A[i];
+	cout<<sum<<" "<<mult;
 }
